83deb87d49f929710a2f24af866539916d33186f
Olga Zaloznaya <zaloznaya.o@udtech.global>
2020-05-15 11:38:17 +0300

    Created libgdx fragment

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,15 +1,37 @@
 package com.mygdx.game;
 
 import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidxFragmentApplication;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends FragmentActivity implements AndroidxFragmentApplication.Callbacks {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Core(), config);
+
+		setContentView(R.layout.layout);
+
+		// Create libgdx fragment
+		GameFragment libgdxFragment = new GameFragment();
+
+		// Put it inside the framelayout (which is defined in the layout.xml file).
+		getSupportFragmentManager().beginTransaction().
+				add(R.id.content_framelayout, libgdxFragment).
+				commit();
+	}
+
+	@Override
+	public void exit() {
+
 	}
 }
+
+//public class AndroidLauncher extends AndroidApplication {
+//	@Override
+//	protected void onCreate (Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+//		initialize(new Core(), config);
+//	}
+//}
---
+++ android/src/com/mygdx/game/GameFragment.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.badlogic.gdx.backends.android.AndroidxFragmentApplication;
+
+public class GameFragment extends AndroidxFragmentApplication {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // return the GLSurfaceView on which libgdx is drawing game stuff
+        return initializeForView(new MyGdxGame());
+    }
+}
+
